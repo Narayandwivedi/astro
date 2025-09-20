07d1e9a9c9fc03ec6611636d13702fa06b4ca134
Narayandwivedi <desinplus1@gmail.com>
2025-09-20 08:04:40 +0530

    fix cors error

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://api.astrosatyaprakash.com', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['https://api.astrosatyaprakash.com', 'https://astrosatyaprakash.com','http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175',],
   credentials: true
 }));
 app.use(express.json());
