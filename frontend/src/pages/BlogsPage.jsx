07b239a5d405a93397f27b550e4e5a916c0add5b
Narayandwivedi <desinplus1@gmail.com>
2026-06-21 23:22:25 +0530

    improve ui

--- frontend/src/pages/BlogsPage.jsx
+++ frontend/src/pages/BlogsPage.jsx
@@ -338,7 +338,7 @@ const BlogsPage = () => {
               </p>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4 md:gap-6">
+            <div className="grid grid-cols-2 lg:grid-cols-5 gap-3 sm:gap-4 md:gap-6">
               {filteredBlogs.map((blog) => (
                 <Link 
                   key={blog._id} 
